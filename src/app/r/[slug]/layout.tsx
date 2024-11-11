7dc2bb0f519552b56571e744edec630c57a36eff
dhiraj02 <dbhandare841@gmail.com>
2024-11-11 15:11:28 +0530

    changed href causing issue

--- src/app/r/[slug]/layout.tsx
+++ src/app/r/[slug]/layout.tsx
@@ -113,7 +113,7 @@ const Layout = async ({
                   variant: 'outline',
                   className: 'w-full mb-6',
                 })}
-                href={`/${slug}/submit`}>
+                href={`/r/${slug}/submit`}>
                 Create Post
               </Link>
             </dl>
